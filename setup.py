028c22e29f8cc813c2b464be4e7df384817580c9
AbeIgnocious <k.l.kubat@gmail.com>
2018-03-16 19:01:49 +0100

    incremented version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-db_locks',
-    version='0.0.4',
+    version='0.0.5',
     packages=['locks', 'locks.management', 'locks.management.commands', 'locks.migrations'],
     url='https://github.com/BlockHub/django-locks',
     license='',
