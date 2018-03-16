7a9391830a582d5d7a7de30427162328f3d0ed98
AbeIgnocious <k.l.kubat@gmail.com>
2018-03-16 17:33:53 +0100

    added requirements.txt, updated setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='django-db_locks',
-    version='0.0.2',
+    version='0.0.3',
     packages=['locks', 'locks.management', 'locks.management.commands', 'locks.migrations'],
     url='https://github.com/BlockHub/django-locks',
     license='',
     author='Charles',
     author_email='karel@blockhub.nl',
     description='easy databaselocking for processes',
-    install_requires=['django']
+    install_requires=['django', 'psycopg2-binary']
 )
